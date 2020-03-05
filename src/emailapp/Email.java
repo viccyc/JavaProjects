bfb7bd1b9a30ee8d9e86edafedce3b26d6ac6282
viccycg <viccyg@digitalpharos.com>
2020-03-05 16:38:15 -0700

    Created method to ask user for and set department

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -13,9 +15,29 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+
+        // Call method to ask for department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+
     }
 
     // Ask for the department
+    private String setDepartment() {
+        // gives options and then captures the input
+        System.out.print("DEPARTMENT CODES:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\n Enter the department code: ");
+        Scanner input = new Scanner(System.in);
+        int deptChoice = input.nextInt();
+        if (deptChoice == 1) {
+            return "sales";
+        } else if (deptChoice == 2) {
+            return "dev";
+        } else if (deptChoice == 3) {
+            return "acct";
+        } else {
+            return "";
+        }
+    }
 
     // Generate a remote password
 
