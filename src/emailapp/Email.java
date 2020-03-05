7ed0d1e43101343dd87a45744194855a46d83b3b
viccycg <viccyg@digitalpharos.com>
2020-03-05 16:22:23 -0700

    Created 2 classes and constructor for the email app

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,4 +1,27 @@
 package emailapp;
 
 public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to receive the first name and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+    }
+
+    // Ask for the department
+
+    // Generate a remote password
+
+    // Set the mailbox capacity
+
+    // Set the alternate email
+
+    // Change the password
 }
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -3,6 +3,6 @@ package emailapp;
 public class EmailApp {
 
     public static void main(String[] args) {
-
+        Email email1 = new Email("Joe", "Bloggs");
     }
 }
