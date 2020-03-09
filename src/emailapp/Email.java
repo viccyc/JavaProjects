134b6c7ad4a49570fd7f0c3f0eb1369c6364274a
viccycg <viccyg@digitalpharos.com>
2020-03-09 15:51:23 -0600

    Created method to generate random password

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -8,6 +8,7 @@ public class Email {
     private String password;
     private String department;
     private int mailboxCapacity;
+    private int defaultPwdLength = 10;
     private String alternateEmail;
 
     // Constructor to receive the first name and last name
@@ -20,12 +21,16 @@ public class Email {
         this.department = setDepartment();
         System.out.println("Department: " + this.department);
 
+        // Call a method that returns a random password
+        this.password = randomPassword(defaultPwdLength);
+        System.out.println("Password generated: " + this.password);
+
     }
 
     // Ask for the department
     private String setDepartment() {
         // gives options and then captures the input
-        System.out.print("DEPARTMENT CODES:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\n Enter the department code: ");
+        System.out.print("DEPARTMENT CODES:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter the department code: ");
         Scanner input = new Scanner(System.in);
         int deptChoice = input.nextInt();
         if (deptChoice == 1) {
@@ -40,6 +45,15 @@ public class Email {
     }
 
     // Generate a remote password
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$#";
+        char[] password = new char[length];
+        for (int i = 0; i < length; i++) {
+            int random = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(random);
+        }
+        return new String(password);
+    }
 
     // Set the mailbox capacity
 
