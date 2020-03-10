6ec6c0ad33194f9c858b7df911c5153547b2cde9
viccycg <viccyg@digitalpharos.com>
2020-03-10 15:19:42 -0600

    Created outline for student class

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,5 +1,28 @@
 package studentdatabaseapp;
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private int studentId;
+    private String course;
+    private int courseCost = 600;
+    private int tuitionBalance;
 
+    // constructor to prompt for student name and year
+    public Student(String firstName, String lastName, int gradeYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradeYear = gradeYear;
+    }
+
+    // create student 5-digit unique ID, first digit is grade level
+
+    // enroll in course
+
+    // get the balance of the student
+
+    // pay tuition
+
+    // show status of student
 }
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -2,6 +2,8 @@ package studentdatabaseapp;
 
 public class StudentDatabaseApp {
     public static void main(String[] args) {
+        // ask how many students will be added to the db
 
+        // Create n number of new students
     }
 }
