4f7ff3e5a6ab315bbbecbcd42b77d7451750c75d
viccycg <viccyg@digitalpharos.com>
2020-03-23 15:32:07 -0600

    Added method to generate account number

--- src/bankaccountapp/Account.java
+++ src/bankaccountapp/Account.java
@@ -1,4 +1,6 @@
 package bankaccountapp;
+import java.util.Random;
+
 
 public abstract class Account implements IBaseRate {
     // List common properties for savings and chequings account
@@ -6,7 +8,9 @@ public abstract class Account implements IBaseRate {
     int sIN;
     double balance;
     double baseRate;
+    static int uniqueNumber = 10000;
     String accountNumber;
+    Random random = new Random();
     // Constructor to set base properties and initialize account
 //    public Account (int baseRate) {
 //        this.baseRate = baseRate;
@@ -20,7 +24,21 @@ public abstract class Account implements IBaseRate {
         System.out.println("SIN Number: " + this.sIN);
         System.out.println("Initial deposit: " + this.balance);
         System.out.print("New Account: ");
+
+        uniqueNumber++;
+        // Set account number
+        this.accountNumber = setAccountNumber();
+        System.out.println("accountNumber: " + this.accountNumber);
+    }
+
+    private String setAccountNumber() {
+        // last 2 digits of SIN, unique 5-digit number and random 3-digit number
+        int lastSinDigits = sIN % 100;
+        int randomDigits = random.nextInt(900) + 100;
+        accountNumber = Integer.toString(lastSinDigits) + Integer.toString(uniqueNumber) + Integer.toString(randomDigits);
+        return accountNumber;
     }
 
     // List common methods
 }
+
