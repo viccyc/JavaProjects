63f8a312581cf80e1a7ae85051c9d633eeb0f51a
viccycg <viccyg@digitalpharos.com>
2020-03-24 16:40:44 -0600

    Created method to assign credit card to the savings account

--- src/bankaccountapp/Chequings.java
+++ src/bankaccountapp/Chequings.java
@@ -2,18 +2,27 @@ package bankaccountapp;
 
 public class Chequings extends Account {
     // List properties specific to a chequings account
-    int debitCardNumber;
+    long debitCardNumber;
     int debitCardPIN;
 
     // Constructor to initialize chequings account properties
     public Chequings(String name, int sIN, double initDeposit) {
         super(name, sIN, initDeposit);
         accountNumber = "1" + accountNumber;
+        assignDebitCard();
+    }
+
+    private void assignDebitCard() {
+        debitCardNumber = (long)(Math.random() * Math.pow(10, 16));
+        debitCardPIN = random.nextInt(9000) + 1000;
     }
 
     // List an methods specific to the chequings account
     public void showAccountInfo() {
         super.showAccountInfo();
-        System.out.println("Type: Chequings account");
+        System.out.println("Chequings account features:" +
+                    "\n Debit card number: " + debitCardNumber +
+                    "\n Card PIN: " + debitCardPIN
+        );
     }
 }
