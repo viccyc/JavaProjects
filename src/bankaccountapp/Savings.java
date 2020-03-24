1f48fed5716b820ee174045b5fb8de804a8dce35
viccycg <viccyg@digitalpharos.com>
2020-03-24 15:57:35 -0600

    Created method to implement savings account safety deposit box

--- src/bankaccountapp/Savings.java
+++ src/bankaccountapp/Savings.java
@@ -9,10 +9,20 @@ public class Savings extends Account {
     public Savings(String name, int sIN, double initDeposit) {
         super(name, sIN, initDeposit);
         accountNumber = "2" + accountNumber;
+        setSafetyDepositBox();
     }
+
+    private void setSafetyDepositBox() {
+        safetyDepositBoxID = random.nextInt(900) + 100;
+        safetyAccessCode = random.nextInt(9000) + 1000;
+    }
+
     // List methods specific to the savings account
     public void showAccountInfo() {
         super.showAccountInfo();
-        System.out.println("Type: Savings account");
+        System.out.println(" Your Savings account features:" +
+                "\n Safety Deposit Box ID: " + safetyDepositBoxID +
+                "\n Safety Access Code: " + safetyAccessCode
+        );
     }
 }
